7aa16784a9dc0365d9d9491966ca4d0146fcf7b9
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-28 09:36:51 +0900

    変更

--- WebEmployeeManagement/Applications/Services/IEmployeeListService.cs
+++ WebEmployeeManagement/Applications/Services/IEmployeeListService.cs
@@ -5,4 +5,5 @@ namespace WebEmployeeManagement.Applications.Services;
 public interface IEmployeeListService
 {
     List<Employee> GetEmployees();
+      Department GetById(int id);
 }
--- WebEmployeeManagement/Applications/Services/Impls/EmployeeListService.cs
+++ WebEmployeeManagement/Applications/Services/Impls/EmployeeListService.cs
@@ -7,15 +7,32 @@ namespace WebEmployeeManagement.Applications.Services.Impls;
 public class EmployeeListService : IEmployeeListService
 {
         private readonly IEmployeeRepository _employeeRepository;
-
-            public EmployeeListService(IEmployeeRepository employeeRepository)
+            private readonly IDepartmentRepository _departmentRepository;
+    public EmployeeListService(
+        AppDbContext context,
+        IEmployeeRepository employeeRepository,
+        IDepartmentRepository departmentRepository)
     {
+
         _employeeRepository = employeeRepository;
+        _departmentRepository = departmentRepository;
     }
+
+
 
         public List<Employee> GetEmployees()
     {
         return _employeeRepository.FindAll();
     }
+        public Department GetById(int id)
+    {
+        var result = _departmentRepository.FindById(id)!;
+        if (result == null)
+        {
+            throw new NotFoundException($"部署Id{id}に該当する部署は存在しません");
+        }
+        return result;
+    }
+
 
 }
